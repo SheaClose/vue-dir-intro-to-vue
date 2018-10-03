b9d3b8bb247cc964e6f5e9c7ff555600ef62dcf6
SheaClose <sheaclose@gmail.com>
2018-10-03 12:32:27 -0500

    done

---
+++ src/components/Child.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div>
+    <h1> {{ name }} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      // type: String,
+      required: true,
+      // default: 'Pass in a string dummy!'
+      validator(val){
+        let types = ['string', 'number'];
+        return types.includes(typeof val);
+      }
+    }
+  }
+}
+</script>
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,75 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h3>{{ test }}</h3>
+    <form @submit.prevent="addName(inputName)">
+      <input type='text' placeholder='Name' v-model='inputName'/>
+      <button @click="addName(inputName)">Submit</button>
+    </form>
+    <button @click="order = '+'">+</button>
+    <button @click="order = '-'">-</button>
+    <br/>
+    <div :key='i' v-for="(e, i) in sortedAdmin">
+      <Child :name="e.name" />
+      <span :title="e.title" v-if='e.name[0] === "c"'>{{ e.name }}</span>
+      <h2 :class="e.name[0] === 'j' ? 'red': 'blue'" v-else>{{e.name}}</h2>
+    </div>
   </div>
 </template>
+
+<script>
+import Child from '@/components/Child';
+
+
+  export default {
+    components:{
+      Child: Child
+    },
+    data(){
+      return {
+        test: 'This is a test of the emergency podcast system',
+        inputName: '',
+        admin: [
+          {name:{}},
+          {name:'joshua'},
+          {name:'cody', title:'Boss!'}
+        ],
+        order: ''
+      }
+    },
+    methods:{
+      addName(name){
+        console.log('e: ', name);
+        this.admin.push({name:name});
+        this.inputName = ''
+      }
+    },
+    computed:{
+      sortedAdmin(){
+        if (!this.order)return this.admin;
+        let sorted = this.admin.slice().sort((a,b)=>a.name.localeCompare(b.name));
+        return this.order === '+' ? sorted : sorted.reverse();
+      }
+    },
+    created(){
+      console.log('created');
+    },
+    mounted(){
+      console.log('mounted');
+    },
+    updated(){
+      console.log('updated');
+    },
+    beforeDestroy(){
+      console.log('destroydd');
+    }    
+  }
+</script>
+
+<style>
+  .red {
+    background: red;
+  }
+  .blue {
+    background: blue;
+  }
+</style>
